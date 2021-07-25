b6fe85b9cdf10815f3ee1816b355807d48cfb38a
safafa <safaaballagh97@gmail.com>
2021-07-25 22:46:34 +0100

    Fix menu icon and logo

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const burgerButton = document.getElementById('burger');
 const closeButton = document.getElementById('close');
 const mobileNav = document.querySelector('.mobile-nav');
+const logo = document.getElementById('logo');
 
 burgerButton.addEventListener('click', () => {
   mobileNav.style.display = 'flex';
+  logo.style.display = 'none';
+  burgerButton.style.display = 'none';
 });
 
 closeButton.addEventListener('click', () => {
   mobileNav.style.display = 'none';
+  logo.style.display = 'block';
+  burgerButton.style.display = 'block';
 });
